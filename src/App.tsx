bf2b9423e1fa518aa128541065437ac9c8c40a19
VInicius Schuelter <48526428+viniciusschuelter@users.noreply.github.com>
2024-01-12 16:17:18 -0300

    feat(dragon): big bang dragon hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css'
+import { useEffect } from 'react';
+import { useDragon } from './hooks/useDragon';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
+    const { dragons, getAllDragons } = useDragon();
+
+    useEffect(() => {
+        getAllDragons()
+    }, [getAllDragons])
 
   return (
     <>
-        big bang
+        dragons
+        {dragons?.map((dragon, index) => (
+            <div>{dragon.name}</div>
+        ))}
+        <ToastContainer />
     </>
   )
 }
---
+++ src/hooks/useDragon.tsx
@@ -0,0 +1,72 @@
+import * as dragonService from '../services/dragon.service';
+import { DragonInterface } from '../interfaces/dragon';
+import { useCallback, useState } from 'react';
+import { toast } from 'react-toastify';
+
+export const useDragon = () => {
+    const [dragons, setDragons] = useState<DragonInterface[]>();
+
+    const toastAlert = (status: number, text?: string) => {
+        status === 200 ?  toast.success(text || 'Success') : toast.error( 'Error');
+    };
+
+    const getAllDragons = useCallback(async () => {
+        try {
+            const { data } = await dragonService.getAllDragons();
+            setDragons(data);
+        } catch (e) {
+            toastAlert(404);
+        }
+    }, []);
+
+    const getDragonById = useCallback(async (id: string) => {
+        try {
+            const { data } = await dragonService.getDragonById(id);
+            return data;
+        } catch (error) {
+            toastAlert(404);
+        }
+    }, []);
+
+    const createNewDragon = useCallback(async (dragon: DragonInterface) => {
+        try {
+            const { status } = await dragonService.createNewDragon(dragon);
+            toastAlert(status, 'Dragon created');
+            return status;
+        } catch (e) {
+            toastAlert(404);
+        }
+    }, []);
+
+    const updateDragonById = useCallback(async (dragon: DragonInterface) => {
+        try {
+            const { status } = await dragonService.updateDragonById(dragon);
+            toastAlert(status, 'Dragon updated');
+            return status;
+        } catch (error) {
+            toastAlert(404);
+        }
+    }, []);
+
+    const deleteDragonById = useCallback(
+        async (id: string) => {
+            try {
+                const { status } = await dragonService.deleteDragonById(id);
+                await getAllDragons();
+                toastAlert(status, 'Dragon deleted');
+            } catch (error) {
+                toastAlert(404);
+            }
+        },
+        [getAllDragons]
+    );
+
+    return {
+        dragons,
+        getAllDragons,
+        getDragonById,
+        createNewDragon,
+        updateDragonById,
+        deleteDragonById,
+    };
+};
